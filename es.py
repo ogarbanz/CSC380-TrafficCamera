4739ca8bcadeade7129403dc5dc392e11a06182f
ogarbanz <60373125+ogarbanz@users.noreply.github.com>
2021-04-24 00:34:20 -0400

    Bug fix

--- es.py
+++ es.py
@@ -1,34 +1,23 @@
-import os
-from datetime import date
-from datetime import datetime
-import smtplib, ssl
-import re
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-
-
-regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
-
-def check(email):
-    if re.search(regex, email):
-        return True
-    else:
-        return False
+from email.mime.text import MIMEText
+from datetime import datetime
+import smtplib
+import ssl
+import os
 
 
 def send_email(receiver_email, measured_speed, location, frame):
-    global user_email
     port = 465
     smtp_server = "smtp.gmail.com"
     sender_email = "sunytrafficcamera@gmail.com"
     password = "OmarJonMichael380"
-    time = str(datetime.time(datetime.now()).hour) + ":" + str(datetime.time(datetime.now()).minute)
+    time = str(datetime.now().strftime("%H:%M"))
 
-    msg = MIMEMultipart('alternative')
+    msg = MIMEMultipart('mixed')
     msg['Subject'] = "Speed Infraction"
     msg['From'] = "SUNY Traffic Camera"
-    msg['To'] = "you"
+    msg['To'] = receiver_email
     img_data = open(frame, 'rb').read()
     html = f"""\
     <html>
@@ -44,18 +33,11 @@ def send_email(receiver_email, measured_speed, location, frame):
     </center>
     </html>"""
 
-    message = MIMEText(html, 'html')
-    msg.attach(message)
-    image = MIMEImage(img_data, name=os.path.basename(frame))
-    msg.attach(image)
+    msg.attach(MIMEText(html, 'html'))
+    msg.attach(MIMEImage(img_data, name=os.path.basename(frame)))
 
-    if check(receiver_email):
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, msg.as_string())
-            server.quit()
-        return True
-    else:
-        print("Invalid Email")
-        return False
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.quit()
